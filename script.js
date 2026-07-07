d064e342c4cab423657f21eeb05116235494a997
GanesaramSuthar <sutharganesaram8@gmail.com>
2026-07-07 18:27:53 +0530

    Update portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// ==========================================
+// Ganesaram Portfolio
+// Version 1.0
+// Author: Ganesaram Suthar
+// ==========================================
+
+console.log("Portfolio Loaded Successfully 🚀");
